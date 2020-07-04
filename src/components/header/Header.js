23427281e7f1fa4073b4803fa3370daefa3a4540
SergeyYakimov <teriertis@yandex.ru>
2020-07-04 22:36:33 +0800

    Made the output of templates

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Header extends ExcelComponent {
-
+  toHtml() {
+    return '<h1>Header</h1>'
+  }
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Table extends ExcelComponent {
-
+  toHtml() {
+    return '<h1>Table</h1>'
+  }
 }
