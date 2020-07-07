00a160053f4db1cdfd2313453d14df300b1bcd84
SergeyYakimov <teriertis@yandex.ru>
2020-07-07 23:06:05 +0800

    Made conditions for selection of group

--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -3,11 +3,13 @@ export class TableSelection {
 
   constructor() {
     this.group = []
+    this.current = null
   }
 
   select($el) {
     this.clear()
     this.group.push($el)
+    this.current = $el
     $el.addClass(TableSelection.className)
   }
 
@@ -16,7 +18,10 @@ export class TableSelection {
     this.group = []
   }
 
-  selectGroup() {
-    return ''
+  selectGroup($group = []) {
+    this.clear()
+    this.group = $group
+
+    this.group.forEach($el => $el.addClass(TableSelection.className))
   }
 }
