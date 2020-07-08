4827dd2c4279f7dfb19a7b56335a46f3d41bf577
SergeyYakimov <teriertis@yandex.ru>
2020-07-08 22:31:51 +0800

    Made the interaction between Formula and Table:

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,10 +3,11 @@ import {ExcelComponent} from '@core/ExcelComponent';
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click']
+      listeners: ['input'],
+      ...options
     });
   }
 
@@ -16,10 +17,7 @@ export class Formula extends ExcelComponent {
   }
 
   onInput(event) {
-    console.log('Formula: onInput', event)
-  }
-
-  onClick() {
-    console.log('Method working!')
+    const text = event.target.textContent.trim()
+    this.emitter.emit('work', text)
   }
 }
