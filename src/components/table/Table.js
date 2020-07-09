9b3155651cf4bd196ba94eadc316fb5bf4215f41
SergeyYakimov <teriertis@yandex.ru>
2020-07-10 01:19:58 +0800

    Made a binding between store and UI

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -37,6 +37,9 @@ export class Table extends ExcelComponent {
     this.$on('formula:done', () => {
       this.selection.current.focus()
     })
+    this.$subscribe(state => {
+      console.log('Table_state', state)
+    })
   }
 
   selectCell($cell) {
@@ -54,7 +57,7 @@ export class Table extends ExcelComponent {
             .map(id => this.$root.find(`[data-id="${id}"]`))
         this.selection.selectGroup($cells)
       } else {
-        this.selection.select($target)
+        this.selectCell($target)
       }
     }
   }
