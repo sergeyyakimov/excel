c391d5714df3a4d716d5ae2152973f31a9038142
SergeyYakimov <teriertis@yandex.ru>
2020-07-11 22:08:04 +0800

    Made ability for the choice of buttons in toolbar

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,8 +1,8 @@
-import {ExcelComponent} from '@core/ExcelComponent';
+import {ExcelStateComponent} from '@core/ExcelStateComponent';
 import {createToolbar} from '@/components/toolbar/toolbar.template';
 import {$} from '@core/dom';
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
     static className = 'excel__toolbar'
 
     constructor($root, options) {
@@ -13,14 +13,30 @@ export class Toolbar extends ExcelComponent {
       });
     }
 
+    prepare() {
+      const initialState = {
+        textAlign: 'left',
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+        textDecoration: 'none'
+      }
+      this.initState(initialState)
+    }
+
+    get template() {
+      return createToolbar(this.state)
+    }
+
     toHtml() {
-      return createToolbar()
+      return this.template
     }
 
     onClick(event) {
       const $target = $(event.target)
       if ($target.data.type === 'button') {
-        console.log($target.data.value)
+        const value = JSON.parse($target.data.value)
+        const key = Object.keys(value)[0]
+        this.setState({[key]: value[key]})
       }
     }
 }
