3915cb302bcc8eaf853031effa9aed9c341b4b17
SergeyYakimov <teriertis@yandex.ru>
2020-07-15 01:53:06 +0800

    Added dashboard and excel pages

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,5 +1,5 @@
 import {$} from '@core/dom';
-import {ActiveRoute} from '@core/routes/ActiveRoute';
+// import {ActiveRoute} from '@core/routes/ActiveRoute';
 
 export class Router {
   constructor(selector, routes) {
@@ -21,7 +21,10 @@ export class Router {
   }
 
   changePageHandler(event) {
-    this.$placeholder.html(ActiveRoute.path)
+    const Page = this.routes.excel
+    const page = new Page()
+    this.$placeholder.append(page.getRoot())
+    page.afterRender()
   }
 
   destroy(event) {
